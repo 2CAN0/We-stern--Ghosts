10f6ad0ca8b4abbb1daccb31cc09f99ea95c2786
2CAN0 <tucaniruito@gmail.com>
2019-07-06 12:43:56 +0200

    Added MiniMap

---
+++ Assets/Scripts/MiniMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public Camera camera;
+    public RenderTexture rTexture;
+    public float zoomOut = 100;
+    public float fov = 80;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera.depth = 0;
+        camera.targetTexture = rTexture;
+        camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y + zoomOut, camera.transform.position.z);
+        camera.fieldOfView = fov;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera.fieldOfView = fov;
+        camera.targetTexture = rTexture;
+    }
+}
---
+++ Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public Vector3 velocity = new Vector3(1, 1, 1);
+    private Vector3 actualVelocity = Vector3.zero;
+    public float amplitude = 5;
+    private int hopCounter = 0;
+    public float jumpForce = 12f;
+    public bool sprint = true;
+    public bool hop = true;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        actualVelocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            actualVelocity.z = velocity.z * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            actualVelocity.z = -velocity.z * Time.deltaTime;
+        }
+        else
+        {
+            actualVelocity.z = 0;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            actualVelocity.x = velocity.x * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            actualVelocity.x = -velocity.x * Time.deltaTime;
+        }
+        else
+        {
+            actualVelocity.x = 0;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            actualVelocity.y += jumpForce;
+        }
+
+        if (hop)
+        {
+            if (hopCounter % amplitude == 0 && hopCounter != 0)
+                velocity.y *= -1;
+
+            if (actualVelocity.magnitude != 0)
+            {
+                actualVelocity.y += velocity.y;
+                hopCounter++;
+            }
+        }
+
+        if (sprint && Input.GetKey(KeyCode.LeftShift)){
+            actualVelocity.z *= 2;
+            actualVelocity.x *= 2;
+        }
+
+        transform.Translate(actualVelocity);
+    }
+}
